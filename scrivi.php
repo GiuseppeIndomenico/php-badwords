c3c58d10c913c99f64d5cce82394767ab8e6d400
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-05-03 14:47:07 +0200

    add trim() and censurate "cazzo"

--- scrivi.php
+++ scrivi.php
@@ -1,6 +1,6 @@
 <?php
-$par = $_POST["par"]
-
+$par = trim($_POST["par"]);
+$parCen = str_replace("cazzo", "*****", $par);
 ?>
 
 
@@ -10,9 +10,24 @@ $par = $_POST["par"]
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Scrittura</title>
 </head>
 <body>
-    <p><?php echo $par ?> </p>
+    <div  class="d-flex align-items-center justify-content-center mt-5 flex-column">
+
+
+        <h1>
+            <?php echo $par ?>
+        </h1>
+        <p class="text-danger">
+            lunghezza frase: <?php echo strlen($par)?> 
+        </p>
+
+        <h1>
+        <?php echo $parCen ?>
+        </h1>
+    </div>
 </body>
 </html>
