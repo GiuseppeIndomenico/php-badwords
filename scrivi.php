3d22ccfcde5b4f1d50faeaf6c33ab6b9aad0a328
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-05-03 14:18:59 +0200

    scaffolding and first test $_post complete

---
+++ scrivi.php
@@ -0,0 +1,18 @@
+<?php
+$par = $_POST["par"]
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scrittura</title>
+</head>
+<body>
+    <p><?php echo $par ?> </p>
+</body>
+</html>
