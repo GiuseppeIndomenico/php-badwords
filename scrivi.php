8426f7d9a9b69b45fdd86ceab89a138de1bd4520
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-05-04 11:40:26 +0200

    add reset button

--- scrivi.php
+++ scrivi.php
@@ -7,24 +7,26 @@ $parCen = str_replace($cen, "***", $par);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Scrittura</title>
 </head>
-<body  class="bg-dark text-light">
-    <div  class="d-flex align-items-center justify-content-center mt-5 flex-column">
+
+<body class="bg-dark text-light">
+    <div class="d-flex align-items-center justify-content-center mt-5 flex-column">
 
 
         <h1>
             <?php echo $parCen ?>
         </h1>
         <p class="text-danger">
-            lunghezza frase: <?php echo strlen($parCen)?> 
+            lunghezza frase: <?php echo strlen($parCen) ?>
         </p>
     </div>
 </body>
+
 </html>
